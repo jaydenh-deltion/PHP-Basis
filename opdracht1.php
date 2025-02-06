076ac90474a0d58107d5aa098af18d023b830af0
jaydenh-deltion <97096023@st.deltion.nl>
2025-02-06 21:08:05 +0100

    bezig snap er niks van

--- opdracht1.php
+++ opdracht1.php
@@ -1,12 +1,10 @@
 <?php
 
-$input= readline(" voer het getal in:");
+for ($i = 1; $i <= 1247; $i++) {
+    echo $i . " * 1247 = " . ($i * 1247) . "<br>";
+}
 
-$getal1= $input;
-$getal2 = 1247;
-
-$result = $getal1 * $getal2;
-
-echo" het getal is $result";
-
+// de for lus is van 1 tot 1247. in de lus is een variable $i om de huidege waarde te geven. 
+// $i * 1247 is de berekening die wordt gebruikt.
+// de echo laat het resultaat zien
 ?>
