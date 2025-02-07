8fbca8295570c0fa4d73ea121e0357a130016b29
jaydenh-deltion <97096023@st.deltion.nl>
2025-02-07 11:36:41 +0100

    werkend maken

--- pizza/index.php
+++ pizza/index.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pizza </title>
 </head>
-<body>
-    <h2></h2>
+<br>
+
+<h2>pizza-ninja's</h2>
+
+    <h3>pizza Seppi</h3>
+
+    <img src="img/Pizza-Seppi.jpg" alt="De Pizza Seppi wat zit er op? tomaten saus, modzarella, champions, ui, paprika en olijven">
+<br></br>
+
+<label> <input type=radio name=" Pizza0" value="0" > Pizza Seppi
+</label>
+
+    <h3>Pizza Tirato</h3>
+
+    <img src="img/Pizza Tirato.jpeg" alt=" De pizza tirato wat zit er op? ui, gehakt, tomaten saus, modzarella, mais en rode peper">
+<br></br>
+
+<label>
+<input type=radio name=" Pizza1" value="1"  > Pizza Tirato
+</label>
+
+    <h3>pizza Spianata Piccante</h3>
+
+    <img src="img/Pizza Spianata Piccante.jpeg" alt=" De pizza Spianata Piccante wat zit er op? tomatensaus, paprika, modzarella,salade en ui">
+<br></br>
+    <label> <input type=radio name=" Pizza3" value="2" > Pizza Spianata Piccante
+</label>
+
+
+<br></br>
+    <label>
+<input type="checkbox" name="Ik wil extra olijven bijbestellen € 2,50" value="0">Ik wil extra olijven bijbestellen € 2,50
+    </label>
+<br></br>
+    <label>
+<input type="checkbox" name="Ik wil extra kaas bijbestellen € 1,50" value="0">Ik wil extra kaas bijbestellen € 1,50
+    </label>
+<br></br>
+    <label>
+<input type="checkbox" name="Ik wil de pizza laten bezorgen € 3,50" value="0">Ik wil de pizza laten bezorgen € 3,50
+    </label>
+    <br></br>
+   <button><label>bestellen</label></button>
+   
 
 <?php
 
