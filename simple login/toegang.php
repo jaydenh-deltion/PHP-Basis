21dd94b5e5de7b538cbc4a6adff52235fc74547d
jaydenh-deltion <97096023@st.deltion.nl>
2025-02-28 18:20:05 +0100

    hash

--- simple login/toegang.php
+++ simple login/toegang.php
@@ -7,25 +7,25 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $password = $_POST['password'];
 
 
-{ $gebruikers = array(
-    array('name'=> 'jan', 'password' => '0000'),
-    array('name' => 'peter', 'password' => '5678'),
-    array('name' => 'hans', 'password' => '9012'),
-    array('name' => 'klaasjan', 'password' => '1111'),
-    array('name' => 'admin', 'password' => '1234'),
-);
-
-foreach($gebruikers as $gebruiker){
-    if($gebruiker['name'] === $username && $gebruiker['password'] === $password){ 
-         echo "ingelogd" . $username;
-         $_SESSION['is_ingelogd'] = 'ingelogt';
+    $gebruikers = array(
+        array('name'=> 'jan', 'password' => hash('sha1', '0000')),
+        array('name' => 'peter', 'password' => hash('sha1', '5678')),
+        array('name' => 'hans', 'password' => hash('sha1', '9012')),
+        array('name' => 'klaasjan', 'password' => hash('sha1', '1111')),
+        array('name' => 'admin', 'password' => hash('sha1', '1234')),
+    );
+    
+    foreach($gebruikers as $gebruiker){
+        if($gebruiker['name'] === $username && hash('sha1', $password) === $gebruiker['password']){ 
+             echo "ingelogd" . $username;
+             $_SESSION['is_ingelogd'] = 'ingelogt';
+        }
     }
-  }
     if(empty($_SESSION['is_ingelogd'])){
         header('location: geen_toegang.php');
     }
 }
-}
+
 ?>
 
 <!DOCTYPE html>
