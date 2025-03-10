da2eb1f4bbe069d05ad4a902815c1ead1eb8211b
jaydenh-deltion <97096023@st.deltion.nl>
2025-03-10 11:34:55 +0100

    werkt steeds minder

--- database login/login.php
+++ database login/login.php
@@ -1,22 +1,34 @@
 <?php
+
 $is_invalid = false;
 
-if ($_["REQUEST_METHOD"] === "POST"){
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
     $mysqli = require __DIR__ . "/database.php";
-
-    $sql =  sprintf("SELECT * FORM user 
+    
+    $sql = sprintf("SELECT * FROM user
                     WHERE email = '%s'",
-                    $mysqli->real_escape_string($_POST["email"]));
-
-    $result =$mysqli->query($sql);
-
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
     $user = $result->fetch_assoc();
-
-    if ($user){
-       if (password_hash($_POST["password"], $user["password_hash"])){
-        die("login successful");
-       }
+    
+    if ($user) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: index.php");
+            exit;
+        }
     }
+    
     $is_invalid = true;
 }
 
